cf483bc6e0dafd5991be7b11751765d76e48fa36
anifyuli <anifyuliansyah@gmail.com>
2022-09-15 22:56:57 +0700

    Change icon and add splash screen with new splash screen API

--- app/src/main/java/com/anifyuli/uenakrecipes/MainActivity.kt
+++ app/src/main/java/com/anifyuli/uenakrecipes/MainActivity.kt
@@ -2,10 +2,13 @@ package com.anifyuli.uenakrecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
